02f44f16abd7bb87c362544ea6bc4c69a7c56e81
otmanTR <r.brk.otman@gmail.com>
2023-03-03 09:49:07 +0300

    add Navbar.js component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import {
+  Routes, Route, Link,
+} from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      
+      <Navbar />
+      
+      <Routes>
+        <Route path="/" element={<Books />} />
+        <Route path="/categories" element={<Categories />} />
+        <Route path="*" element={<div>NotFound</div>} />
+      </Routes>
+    </>
   );
 }
 
 export default App;
+
---
+++ src/components/Navbar.js
@@ -0,0 +1,22 @@
+import {
+    Routes, Route, Link,
+  } from 'react-router-dom';
+export default function Navbar() {
+    return (
+        <>
+        <div className="app">
+          <h1>Bookstore</h1>
+        <nav>
+          <ul className="navContainer">
+            <li>
+              <Link to="/">Books</Link>
+            </li>
+            <li>
+              <Link to="/categories">Categories</Link>
+            </li>
+          </ul>
+        </nav>
+        </div>
+        </>
+    );
+}
