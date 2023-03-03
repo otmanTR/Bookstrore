61d36b986676d0c932632483e5dafcad7dc14b83
otmanTR <r.brk.otman@gmail.com>
2023-03-03 12:05:45 +0300

    add Categories.js component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {
 import './App.css';
 import Navbar from './components/Navbar';
 import Books from './components/Books';
+import Categories from './components/Categories';
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Books />} />
-        {/* <Route path="/categories" element={<Categories />} /> */}
+        <Route path="/categories" element={<Categories />} />
         <Route path="*" element={<div>NotFound</div>} />
       </Routes>
     </>
