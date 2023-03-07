673602571a17a55c0a23efc87ba3bb013232403b
otmanTR <r.brk.otman@gmail.com>
2023-03-07 16:10:45 +0300

    add booksSlice functions

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import booksReducer from './books/booksSlice';
 
-export const store = configureStore({
-  reducer: {},
+const store = configureStore({
+  reducer: {
+    books: booksReducer,
+  },
 });
+
+export default store;
