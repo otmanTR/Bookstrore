d5d0065dcf5afb8ff68ba7b69794cf84fa64a6be
otmanTR <r.brk.otman@gmail.com>
2023-03-08 16:56:43 +0300

    add event add to add button

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,28 @@
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addBook } from '../redux/books/booksSlice';
+
 export default function Form() {
+  const dispatch = useDispatch();
+  const [newTitle, setNewTitle] = useState('');
+  const [newAuthor, setNewAuthor] = useState('');
+
+  const clickAddBook = (e) => {
+    e.preventDefault();
+    dispatch(addBook(
+      {
+        title: newTitle,
+        author: newAuthor,
+      },
+    ));
+  };
   return (
     <>
       <form>
         <h3>Adding a Book</h3>
-        <input placeholder="title" />
-        <input placeholder="author" />
+        <input type="text" placeholder="title" required onChange={(e) => setNewTitle(e.target.value)} />
+        <input type="text" placeholder="author" required onChange={(e) => setNewAuthor(e.target.value)} />
+        <button type="submit" onClick={clickAddBook}>Add Book</button>
       </form>
     </>
   );
